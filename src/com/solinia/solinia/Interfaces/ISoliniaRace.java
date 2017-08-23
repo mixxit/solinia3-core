7dfbd7dac5f4b9ee778ba4c068bf2daa3afa638c
mixxit <michaelbollanduk@gmail.com>
2017-08-23 22:08:00 +0100

    Racial Base Stats

--- src/com/solinia/solinia/Interfaces/ISoliniaRace.java
+++ src/com/solinia/solinia/Interfaces/ISoliniaRace.java
@@ -8,4 +8,33 @@ public interface ISoliniaRace {
 
 	public int getId();
 
+	void setAdmin(boolean isadmin);
+
+	void setAgility(int agility);
+
+	void setCharisma(int charisma);
+
+	void setDexterity(int dexterity);
+
+	void setIntelligence(int intelligence);
+
+	void setStamina(int stamina);
+
+	void setStrength(int strength);
+
+	void setWisdom(int wisdom);
+
+	public int getAgility();
+
+	public int getCharisma();
+
+	public int getDexterity();
+
+	public int getIntelligence();
+
+	public int getStamina();
+
+	public int getStrength();
+
+	public int getWisdom();
 }
--- src/com/solinia/solinia/Models/SoliniaRace.java
+++ src/com/solinia/solinia/Models/SoliniaRace.java
@@ -8,6 +8,14 @@ public class SoliniaRace implements ISoliniaRace {
 	private boolean isadmin;
 	private String name;
 	
+	private int strength;
+	private int stamina;
+	private int agility;
+	private int dexterity;
+	private int wisdom;
+	private int intelligence;
+	private int charisma;
+	
 	@Override
 	public String getName() {
 		// TODO Auto-generated method stub
@@ -21,9 +29,84 @@ public class SoliniaRace implements ISoliniaRace {
 	}
 
 	@Override
+	public void setAdmin(boolean isadmin) {
+		// TODO Auto-generated method stub
+		this.isadmin = isadmin;
+	}
+	
+	@Override
 	public int getId() {
 		// TODO Auto-generated method stub
 		return this.id;
 	}
 
+	@Override
+	public int getStrength() {
+		return strength;
+	}
+
+	@Override
+	public void setStrength(int strength) {
+		this.strength = strength;
+	}
+
+	@Override
+	public int getStamina() {
+		return stamina;
+	}
+
+	@Override
+	public void setStamina(int stamina) {
+		this.stamina = stamina;
+	}
+
+	@Override
+	public int getAgility() {
+		return agility;
+	}
+
+	@Override
+	public void setAgility(int agility) {
+		this.agility = agility;
+	}
+
+	@Override
+	public int getDexterity() {
+		return dexterity;
+	}
+
+	@Override
+	public void setDexterity(int dexterity) {
+		this.dexterity = dexterity;
+	}
+
+	@Override
+	public int getWisdom() {
+		return wisdom;
+	}
+
+	@Override
+	public void setWisdom(int wisdom) {
+		this.wisdom = wisdom;
+	}
+
+	@Override
+	public int getIntelligence() {
+		return intelligence;
+	}
+
+	@Override
+	public void setIntelligence(int intelligence) {
+		this.intelligence = intelligence;
+	}
+
+	@Override
+	public int getCharisma() {
+		return charisma;
+	}
+
+	@Override
+	public void setCharisma(int charisma) {
+		this.charisma = charisma;
+	}
 }
