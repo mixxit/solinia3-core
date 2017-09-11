0435c2dcccec2a5d6b237290d1c4bcbc02dcfd83
mixxit <michaelbollanduk@gmail.com>
2017-09-11 20:25:27 +0100

    set npcs as default skeleton type so they do not (strangely?) assist other zombie types (aitargetselectors should be observed)

--- src/com/solinia/solinia/Models/SoliniaNPC.java
+++ src/com/solinia/solinia/Models/SoliniaNPC.java
@@ -25,7 +25,7 @@ import net.md_5.bungee.chat.ComponentSerializer;
 public class SoliniaNPC implements ISoliniaNPC {
 	private int id;
 	private String name;
-	private String mctype = "ZOMBIE";
+	private String mctype = "SKELETON"; // do not use zombie, it ignores assist rules
 	private int level = 1;
 	private int factionid;
 	private boolean usedisguise = false;
