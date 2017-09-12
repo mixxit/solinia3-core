027b686b20b4ba444e1bd5e20820799b6ea0853e
mixxit <michaelbollanduk@gmail.com>
2017-09-12 12:55:27 +0100

    Allow console to create items

--- src/com/solinia/solinia/Commands/CommandCreateItem.java
+++ src/com/solinia/solinia/Commands/CommandCreateItem.java
@@ -5,6 +5,7 @@ import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
@@ -15,38 +16,49 @@ import com.solinia.solinia.Interfaces.ISoliniaItem;
 public class CommandCreateItem implements CommandExecutor {
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if (!(sender instanceof Player))
+		if (!(sender instanceof Player) && !(sender instanceof ConsoleCommandSender))
 			return false;
 		
-		Player player = (Player)sender;
-		if (!player.isOp())
+		if (sender instanceof Player)
 		{
-			player.sendMessage("This is an operator only command");
-			return false;
+			Player player = (Player)sender;
+			if (!player.isOp())
+			{
+				player.sendMessage("This is an operator only command");
+				return false;
+			}
 		}
 		
-		ItemStack itemstack = player.getInventory().getItemInMainHand();
+		ItemStack itemstack = null;
         
-        if (itemstack.getType().equals(Material.AIR))
-        {
-        	player.sendMessage(ChatColor.GRAY+"Empty item in hand");
-        	return false;
-        }
+		if (args.length == 0 && sender instanceof Player)
+		{
+			Player player = (Player)sender;
+			itemstack = player.getInventory().getItemInMainHand();
+	        if (itemstack.getType().equals(Material.AIR))
+	        {
+	        	player.sendMessage(ChatColor.GRAY+"Empty item in hand");
+	        	return false;
+	        }
+	        
+	        if (itemstack.getEnchantmentLevel(Enchantment.OXYGEN) > 999)
+	        {
+	        	player.sendMessage("You can only create a new item from core minecraft items, not solinia items");
+	        	return true;
+	        }
+		} else {
+			itemstack = new ItemStack(Material.valueOf(args[0]));
+		}
+		
+        sender.sendMessage("Building new item based on: " + itemstack.getType().name());
         
-        if (itemstack.getEnchantmentLevel(Enchantment.OXYGEN) > 999)
-        {
-        	player.sendMessage("You can only create a new item from core minecraft items, not solinia items");
-        	return true;
-        }
-        
-        player.sendMessage("Building new item based on: " + itemstack.getType().name());
         try
         {
         	ISoliniaItem item = SoliniaItemFactory.CreateItem(itemstack);
-        	player.sendMessage("New Item Created with ID: " + item.getId());
+        	sender.sendMessage("New Item Created with ID: " + item.getId());
         } catch (Exception e)
         {
-        	player.sendMessage(e.getMessage());
+        	sender.sendMessage(e.getMessage());
         }
 
 		return true;
