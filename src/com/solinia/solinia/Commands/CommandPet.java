611b02abe90c510c2c2d755fc75ae7981b090672
Michael Bolland <michaelbollanduk@gmail.com>
2018-01-17 13:02:18 +0000

    Show AC, Pet Buffs on /pet command (Closes #53)

--- src/com/solinia/solinia/Commands/CommandPet.java
+++ src/com/solinia/solinia/Commands/CommandPet.java
@@ -8,10 +8,19 @@ import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Wolf;
 
+import com.solinia.solinia.Adapters.SoliniaLivingEntityAdapter;
 import com.solinia.solinia.Exceptions.CoreStateInitException;
+import com.solinia.solinia.Interfaces.ISoliniaLivingEntity;
 import com.solinia.solinia.Interfaces.ISoliniaNPC;
+import com.solinia.solinia.Interfaces.ISoliniaSpell;
 import com.solinia.solinia.Managers.StateManager;
+import com.solinia.solinia.Models.SoliniaActiveSpell;
+import com.solinia.solinia.Models.SoliniaEntitySpells;
+import com.solinia.solinia.Models.SoliniaLivingEntity;
 
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.TextComponent;
 import net.minecraft.server.v1_12_R1.EntityInsentient;
 import net.minecraft.server.v1_12_R1.GenericAttributes;
 
@@ -46,9 +55,36 @@ public class CommandPet implements CommandExecutor {
 				
 				player.sendMessage("Pet Name: " + pet.getName());
 				player.sendMessage("Pet HP: " + pet.getHealth() + "/" + pet.getMaxHealth());
+				ISoliniaLivingEntity petLivingEntity = SoliniaLivingEntityAdapter.Adapt(pet);
+				player.sendMessage("Pet AC: " + petLivingEntity.getMitigationAC());
+				
 				EntityInsentient entityhandle = (EntityInsentient) ((org.bukkit.craftbukkit.v1_12_R1.entity.CraftLivingEntity) pet).getHandle();
 				double dmg = entityhandle.getAttributeInstance(GenericAttributes.ATTACK_DAMAGE).getValue();
 				player.sendMessage("Pet DMG: " + dmg);
+				player.sendMessage("Active Effects:");
+				SoliniaEntitySpells spells = StateManager.getInstance().getEntityManager().getActiveEntitySpells(pet);
+				for(SoliniaActiveSpell activeSpell : spells.getActiveSpells())
+	            {
+	            	ISoliniaSpell spell = StateManager.getInstance().getConfigurationManager().getSpell(activeSpell.getSpellId());
+	            	String removetext = "";
+	            	ChatColor spellcolor = ChatColor.GREEN;
+	            	if (spell.isBeneficial())
+	            	{
+	            		removetext = "Removable spell";
+	            	} else {
+	            		removetext = "Unremovable spell";
+	            		spellcolor = ChatColor.RED;
+	            	}
+	            	
+	            	TextComponent tc = new TextComponent();
+					tc.setText("- " + spellcolor + spell.getName() + ChatColor.RESET + " " + activeSpell.getTicksLeft() + " ticks left - ");
+					TextComponent tc2 = new TextComponent();
+					tc2.setText(removetext);
+					tc.addExtra(tc2);
+					sender.spigot().sendMessage(tc);	
+	            	
+	            }
+				
 				return true;
 			} catch (CoreStateInitException e)
 			{
