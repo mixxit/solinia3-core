12679ec6c62c8d2547a6ce8b209fbd5f202f2fe6
Michael Bolland <michaelbollanduk@gmail.com>
2018-04-14 10:42:21 +0100

    C:/Program Files/Git/equip equip and /equip unequip

--- src/com/solinia/solinia/Commands/CommandEquip.java
+++ src/com/solinia/solinia/Commands/CommandEquip.java
@@ -15,6 +15,7 @@ import com.solinia.solinia.Exceptions.SoliniaItemException;
 import com.solinia.solinia.Interfaces.ISoliniaItem;
 import com.solinia.solinia.Interfaces.ISoliniaPlayer;
 import com.solinia.solinia.Managers.StateManager;
+import com.solinia.solinia.Models.SoliniaAccountClaim;
 import com.solinia.solinia.Utils.Utils;
 
 public class CommandEquip implements CommandExecutor {
@@ -23,160 +24,211 @@ public class CommandEquip implements CommandExecutor {
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (!(sender instanceof Player) && !(sender instanceof CommandSender))
 			return false;
-		
-		Player player = (Player)sender;
+
 		try {
+			Player player = (Player) sender;
 			ISoliniaPlayer solPlayer = SoliniaPlayerAdapter.Adapt(player);
+
+			if (args.length < 1) {
+				showCurrentEquippedItems(solPlayer);
+				sender.sendMessage("Insufficient arugments  - /equip equip , /equip unequip");
+				return true;
+			}
+
+			if (args[0].toUpperCase().equals("EQUIP")) {
+				try {
+					showCurrentEquippedItems(solPlayer);
+
+					ItemStack primaryItem = player.getInventory().getItemInMainHand();
+					if (primaryItem.getType().equals(Material.AIR)) {
+						player.sendMessage(ChatColor.GRAY
+								+ "Empty item in primary hand. You must hold the item you want to equip in your main hand");
+						return false;
+					}
+
+					if (!Utils.IsSoliniaItem(primaryItem)) {
+						player.sendMessage("You can only equip solinia items this way");
+						return true;
+					}
+
+					ISoliniaItem item = SoliniaItemAdapter.Adapt(primaryItem);
+					if (item == null) {
+						player.sendMessage("You cannot equip this item this way");
+						return true;
+					}
+
+					if (item.getMinLevel() > solPlayer.getLevel()) {
+						player.sendMessage("You cannot equip this item (minlevel: " + item.getMinLevel() + ")");
+						return true;
+					}
+
+					if (solPlayer.getClassObj() == null) {
+						player.sendMessage(ChatColor.GRAY + "Your class cannot wear this equipment");
+						return true;
+					}
+
+					if (item.isSpellscroll()) {
+						player.sendMessage("You cannot equip this item");
+						return true;
+					}
+
+					if (item.getAllowedClassNames() != null && item.getAllowedClassNames().size() > 0)
+						if (!item.getAllowedClassNames().contains(solPlayer.getClassObj().getName().toUpperCase())) {
+							player.sendMessage(ChatColor.GRAY + "Your class cannot wear this equipment");
+							return true;
+						}
+
+					if (item.getMinLevel() > solPlayer.getLevel()) {
+						player.sendMessage(ChatColor.GRAY + "Your are not sufficient level to wear this equipment");
+						return true;
+					}
+
+					if (item.isFingersItem())
+						if (solPlayer.getFingersItem() > 0) {
+							player.sendMessage("You have already equipped an item in that slot");
+							return true;
+						} else {
+							solPlayer.setFingersItem(item.getId());
+							player.getInventory().setItemInMainHand(null);
+							player.updateInventory();
+							player.sendMessage("You have equipped this item");
+							return true;
+						}
+
+					if (item.isShouldersItem())
+						if (solPlayer.getShouldersItem() > 0) {
+							player.sendMessage("You have already equipped an item in that slot");
+							return true;
+						} else {
+							solPlayer.setShouldersItem(item.getId());
+							player.getInventory().setItemInMainHand(null);
+							player.updateInventory();
+							player.sendMessage("You have equipped this item");
+							return true;
+						}
+
+					if (item.isEarsItem())
+						if (solPlayer.getEarsItem() > 0) {
+							player.sendMessage("You have already equipped an item in that slot");
+							return true;
+						} else {
+							solPlayer.setEarsItem(item.getId());
+							player.getInventory().setItemInMainHand(null);
+							player.updateInventory();
+							player.sendMessage("You have equipped this item");
+							return true;
+						}
+
+					if (item.isNeckItem())
+						if (solPlayer.getNeckItem() > 0) {
+							player.sendMessage("You have already equipped an item in that slot");
+							return true;
+						} else {
+							solPlayer.setNeckItem(item.getId());
+							player.getInventory().setItemInMainHand(null);
+							player.updateInventory();
+							player.sendMessage("You have equipped this item");
+							return true;
+						}
+
+					return false;
+
+				} catch (SoliniaItemException e) {
+					player.sendMessage("You cannot equip this item");
+					return true;
+				}
+			}
+
+			if (args[0].toUpperCase().equals("UNEQUIP")) {
+
+				showCurrentEquippedItems(solPlayer);
+				if (solPlayer.getEarsItem() > 0) {
+					SoliniaAccountClaim newclaim = new SoliniaAccountClaim();
+					newclaim.setId(StateManager.getInstance().getConfigurationManager().getNextAccountClaimId());
+					newclaim.setMcname(player.getName());
+					newclaim.setItemid(solPlayer.getEarsItem());
+					newclaim.setClaimed(false);
+					StateManager.getInstance().getConfigurationManager().addAccountClaim(newclaim);
+					solPlayer.setEarsItem(0);
+					sender.sendMessage("Your earrings have been moved to the /claim inventory");
+				}
+				if (solPlayer.getNeckItem() > 0) {
+					SoliniaAccountClaim newclaim = new SoliniaAccountClaim();
+					newclaim.setId(StateManager.getInstance().getConfigurationManager().getNextAccountClaimId());
+					newclaim.setMcname(player.getName());
+					newclaim.setItemid(solPlayer.getEarsItem());
+					newclaim.setClaimed(false);
+					StateManager.getInstance().getConfigurationManager().addAccountClaim(newclaim);
+					solPlayer.setNeckItem(0);
+					sender.sendMessage("Your necklace has been moved to the /claim inventory");
+				}
+				if (solPlayer.getFingersItem() > 0) {
+					SoliniaAccountClaim newclaim = new SoliniaAccountClaim();
+					newclaim.setId(StateManager.getInstance().getConfigurationManager().getNextAccountClaimId());
+					newclaim.setMcname(player.getName());
+					newclaim.setItemid(solPlayer.getFingersItem());
+					newclaim.setClaimed(false);
+					StateManager.getInstance().getConfigurationManager().addAccountClaim(newclaim);
+					solPlayer.setFingersItem(0);
+					sender.sendMessage("Your rings have been moved to the /claim inventory");
+				}
+				if (solPlayer.getShouldersItem() > 0) {
+					SoliniaAccountClaim newclaim = new SoliniaAccountClaim();
+					newclaim.setId(StateManager.getInstance().getConfigurationManager().getNextAccountClaimId());
+					newclaim.setMcname(player.getName());
+					newclaim.setItemid(solPlayer.getShouldersItem());
+					newclaim.setClaimed(false);
+					StateManager.getInstance().getConfigurationManager().addAccountClaim(newclaim);
+					solPlayer.setShouldersItem(0);
+					sender.sendMessage("Your cloak has been moved to the /claim inventory");
+				}
+
+				return true;
+
+			}
+
 			showCurrentEquippedItems(solPlayer);
-			
-			ItemStack primaryItem = player.getInventory().getItemInMainHand();
-	        if (primaryItem.getType().equals(Material.AIR))
-	        {
-	        	player.sendMessage(ChatColor.GRAY+"Empty item in primary hand. You must hold the item you want to equip in your main hand");
-	        	return false;
-	        }
-	        
-	        if (!Utils.IsSoliniaItem(primaryItem))
-	        {
-	        	player.sendMessage("You can only equip solinia items this way");
-	        	return true;
-	        }
-	        
-	        ISoliniaItem item = SoliniaItemAdapter.Adapt(primaryItem);
-	        if (item == null)
-	        {
-	        	player.sendMessage("You cannot equip this item this way");
-	        	return true;
-	        }
-	        
-	        if (item.getMinLevel() > solPlayer.getLevel())
-	        {
-	        	player.sendMessage("You cannot equip this item (minlevel: " + item.getMinLevel() + ")");
-	        	return true;
-	        }
-	        
-	        if (solPlayer.getClassObj() == null)
-    		{
-	        	player.sendMessage(ChatColor.GRAY + "Your class cannot wear this equipment");
-    			return true;
-    		}
-	        
-	        if (item.isSpellscroll())
-	        {
-	        	player.sendMessage("You cannot equip this item");
-	        	return true;
-	        }
 
-	        if (item.getAllowedClassNames() != null && item.getAllowedClassNames().size() > 0)
-    		if (!item.getAllowedClassNames().contains(solPlayer.getClassObj().getName().toUpperCase()))
-    		{
-    			player.sendMessage(ChatColor.GRAY + "Your class cannot wear this equipment");
-    			return true;
-    		}
-	        
-	        if (item.getMinLevel() > solPlayer.getLevel())
-    		{
-    			player.sendMessage(ChatColor.GRAY + "Your are not sufficient level to wear this equipment");
-    			return true;
-    		}
-	        
-	        if (item.isFingersItem())
-	        if (solPlayer.getFingersItem() > 0)
-	        {
-	        	player.sendMessage("You have already equipped an item in that slot");
-	        	return true;
-	        } else {
-	        	solPlayer.setFingersItem(item.getId());
-	        	player.getInventory().setItemInMainHand(null);
-	        	player.updateInventory();
-	        	player.sendMessage("You have equipped this item");
-	        	return true;
-	        }
-
-	        if (item.isShouldersItem())
-	        if (solPlayer.getShouldersItem() > 0)
-	        {
-	        	player.sendMessage("You have already equipped an item in that slot");
-	        	return true;
-	        } else {
-	        	solPlayer.setShouldersItem(item.getId());
-	        	player.getInventory().setItemInMainHand(null);
-	        	player.updateInventory();
-	        	player.sendMessage("You have equipped this item");
-	        	return true;
-	        }
-	        
-	        if (item.isEarsItem())
-		        if (solPlayer.getEarsItem() > 0)
-		        {
-		        	player.sendMessage("You have already equipped an item in that slot");
-		        	return true;
-		        } else {
-		        	solPlayer.setEarsItem(item.getId());
-		        	player.getInventory().setItemInMainHand(null);
-		        	player.updateInventory();
-		        	player.sendMessage("You have equipped this item");
-		        	return true;
-		        }
-
-	        if (item.isNeckItem())
-	        if (solPlayer.getNeckItem() > 0)
-	        {
-	        	player.sendMessage("You have already equipped an item in that slot");
-	        	return true;
-	        } else {
-	        	solPlayer.setNeckItem(item.getId());
-	        	player.getInventory().setItemInMainHand(null);
-	        	player.updateInventory();
-	        	player.sendMessage("You have equipped this item");
-	        	return true;
-	        }
-	        
-			return false;
 		} catch (CoreStateInitException e) {
-			return false;
-		} catch (SoliniaItemException e) {
-        	player.sendMessage("You cannot equip this item");
-        	return true;
+
 		}
+		return true;
 	}
 
 	private void showCurrentEquippedItems(ISoliniaPlayer solPlayer) {
-		try
-		{
-			if (solPlayer.getNeckItem() > 0)
-			{
-				ISoliniaItem item = StateManager.getInstance().getConfigurationManager().getItem(solPlayer.getNeckItem());
+		try {
+			if (solPlayer.getNeckItem() > 0) {
+				ISoliniaItem item = StateManager.getInstance().getConfigurationManager()
+						.getItem(solPlayer.getNeckItem());
 				solPlayer.getBukkitPlayer().sendMessage("Neck Item: " + item.getDisplayname());
 			} else {
 				solPlayer.getBukkitPlayer().sendMessage("Neck Item: EMPTY");
 			}
-			if (solPlayer.getFingersItem() > 0)
-			{
-				ISoliniaItem item = StateManager.getInstance().getConfigurationManager().getItem(solPlayer.getFingersItem());
+			if (solPlayer.getFingersItem() > 0) {
+				ISoliniaItem item = StateManager.getInstance().getConfigurationManager()
+						.getItem(solPlayer.getFingersItem());
 				solPlayer.getBukkitPlayer().sendMessage("Fingers Item: " + item.getDisplayname());
 			} else {
 				solPlayer.getBukkitPlayer().sendMessage("Fingers Item: EMPTY");
 			}
-			
-			if (solPlayer.getEarsItem() > 0)
-			{
-				ISoliniaItem item = StateManager.getInstance().getConfigurationManager().getItem(solPlayer.getEarsItem());
+
+			if (solPlayer.getEarsItem() > 0) {
+				ISoliniaItem item = StateManager.getInstance().getConfigurationManager()
+						.getItem(solPlayer.getEarsItem());
 				solPlayer.getBukkitPlayer().sendMessage("Ears Item: " + item.getDisplayname());
 			} else {
 				solPlayer.getBukkitPlayer().sendMessage("Ears Item: EMPTY");
 			}
-			
-			if (solPlayer.getShouldersItem() > 0)
-			{
-				ISoliniaItem item = StateManager.getInstance().getConfigurationManager().getItem(solPlayer.getShouldersItem());
+
+			if (solPlayer.getShouldersItem() > 0) {
+				ISoliniaItem item = StateManager.getInstance().getConfigurationManager()
+						.getItem(solPlayer.getShouldersItem());
 				solPlayer.getBukkitPlayer().sendMessage("Shoulders Item: " + item.getDisplayname());
 			} else {
 				solPlayer.getBukkitPlayer().sendMessage("Shoulders Item: EMPTY");
 			}
-		} catch (CoreStateInitException e)
-		{
-			
+		} catch (CoreStateInitException e) {
+
 		}
 	}
 }
