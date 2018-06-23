40df4fe414fbcf2fd1abc267cd21817e65457f90
mixxit <michaelbollanduk@gmail.com>
2018-06-23 20:14:26 +0100

    correct auto attack animation

--- src/com/solinia/solinia/Models/SoliniaLivingEntity.java
+++ src/com/solinia/solinia/Models/SoliniaLivingEntity.java
@@ -164,6 +164,19 @@ public class SoliniaLivingEntity implements ISoliniaLivingEntity {
 
 		if (getBukkitLivingEntity() instanceof Player)
 		{
+			net.minecraft.server.v1_12_R1.Entity ep = ((CraftEntity) getBukkitLivingEntity()).getHandle();
+			PacketPlayOutAnimation packet = new PacketPlayOutAnimation(ep, 0);
+			getBukkitLivingEntity().getWorld().playSound(getBukkitLivingEntity().getLocation(), Sound.ENTITY_PLAYER_ATTACK_STRONG, 1.0F,
+					1.0F);
+			
+			for (Entity listening : getBukkitLivingEntity().getNearbyEntities(20, 20, 20)) {
+				if (listening instanceof Player)
+					((CraftPlayer) listening).getHandle().playerConnection.sendPacket(packet);
+			}
+			
+			if (getBukkitLivingEntity() instanceof Player)
+			((CraftPlayer) getBukkitLivingEntity()).getHandle().playerConnection.sendPacket(packet);
+			
 			((CraftPlayer) getBukkitLivingEntity()).getHandle()
 				.attack(((CraftEntity) defender.getBukkitLivingEntity()).getHandle());
 		} else {
