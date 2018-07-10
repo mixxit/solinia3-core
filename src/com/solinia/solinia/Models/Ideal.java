362c2d30a9dbb83af301ddc998d04b3aab76abcb
mixxit <michaelbollanduk@gmail.com>
2018-07-10 22:35:00 +0100

    ideal ids

--- src/com/solinia/solinia/Models/Ideal.java
+++ src/com/solinia/solinia/Models/Ideal.java
@@ -1,10 +1,12 @@
 package com.solinia.solinia.Models;
 
 public class Ideal {
+	int id;
 	String ideal;
 	String description;
 	AlignmentType alignmentType;
-	public Ideal(String ideal, String description, AlignmentType alignmentType) {
+	public Ideal(int id,String ideal, String description, AlignmentType alignmentType) {
+		this.id = id;
 		this.ideal = ideal;
 		this.description = description;
 		this.alignmentType = alignmentType;
