2faf36b6cc8ac751d13884fd407eead63de0b9f3
Michael Bolland <michaelbollanduk@gmail.com>
2018-12-03 12:54:01 +0000

    /playeremote admin command

--- src/com/solinia/solinia/Commands/CommandPlayerEmote.java
+++ src/com/solinia/solinia/Commands/CommandPlayerEmote.java
@@ -10,7 +10,7 @@ import com.solinia.solinia.Adapters.SoliniaPlayerAdapter;
 import com.solinia.solinia.Exceptions.CoreStateInitException;
 import com.solinia.solinia.Interfaces.ISoliniaPlayer;
 
-public class CommandGrantTitle implements CommandExecutor {
+public class CommandPlayerEmote implements CommandExecutor {
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (!(sender instanceof Player) && !(sender instanceof CommandSender))
@@ -22,7 +22,7 @@ public class CommandGrantTitle implements CommandExecutor {
 		if (sender instanceof Player)
 		{
 			Player player = (Player)sender;
-			if (!player.isOp() && !player.hasPermission("solinia.granttitle"))
+			if (!player.isOp() && !player.hasPermission("solinia.playeremote"))
 			{
 				player.sendMessage("You do not have permission to access this command");
 				return true;
@@ -38,25 +38,22 @@ public class CommandGrantTitle implements CommandExecutor {
 			return true;
 		}
 		
-		String targetTitle = "";
+		String emote = "";
 		int current = 0;
 		for (String entry : args) {
 			current++;
 			if (current < 2)
 				continue;
 			
-			targetTitle = targetTitle + entry + " ";
+			emote = emote + entry + " ";
 		}
 
-		targetTitle = targetTitle.trim();
+		emote = emote.trim();
 		
 		try {
 			ISoliniaPlayer solplayer = SoliniaPlayerAdapter.Adapt(Bukkit.getPlayer(args[0]));
-			boolean success = solplayer.grantTitle(targetTitle);
-			if (success)
-				sender.sendMessage("Player title granted: " + targetTitle);
-			else 
-				sender.sendMessage("Player already has title: " + targetTitle);
+			solplayer.emote(emote, false);
+			sender.sendMessage("Forced player emote");
 				
 			return true;
 		} catch (CoreStateInitException e) {
