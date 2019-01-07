7bf997d673249ed288cea6ffccb10d7856c10138
Michael Bolland <michaelbollanduk@gmail.com>
2019-01-07 23:04:27 +0000

    remove temp item dmg patch

--- src/com/solinia/solinia/Managers/CoreState.java
+++ src/com/solinia/solinia/Managers/CoreState.java
@@ -544,11 +544,6 @@ public class CoreState {
 			
 			for(ISoliniaItem item : StateManager.getInstance().getConfigurationManager().getItems())
 			{
-				if (item.getDamage() > 0 && item.getDamage() < 6)
-				{
-					item.setDamage(6);
-				}
-				
 				if (item.isHalfening2() == true)
 					continue;
 				
