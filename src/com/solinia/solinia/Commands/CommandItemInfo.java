5584ec52036fc339028e6c72d8e86674b745f9bd
Michael Bolland <michaelbollanduk@gmail.com>
2019-01-12 09:59:42 +0000

    iteminfo command

---
+++ src/com/solinia/solinia/Commands/CommandItemInfo.java
@@ -0,0 +1,44 @@
+package com.solinia.solinia.Commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import com.solinia.solinia.Adapters.ItemStackAdapter;
+import com.solinia.solinia.Adapters.SoliniaItemAdapter;
+import com.solinia.solinia.Exceptions.CoreStateInitException;
+import com.solinia.solinia.Exceptions.SoliniaItemException;
+import com.solinia.solinia.Interfaces.ISoliniaItem;
+
+public class CommandItemInfo implements CommandExecutor {
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (!(sender instanceof Player))
+		{
+			sender.sendMessage("This is a player only command");
+			return true;
+		}
+		
+		sender.sendMessage("Fetching information about item in main hand");
+		Player player = (Player)sender;
+		ItemStack itemStack = player.getEquipment().getItemInMainHand();
+		player.sendMessage("Material: " + itemStack.getType().toString());
+		player.sendMessage("Durability: " + itemStack.getDurability());
+		player.sendMessage("Amount: " + itemStack.getAmount());
+		
+		try
+		{
+			ISoliniaItem solItem = SoliniaItemAdapter.Adapt(itemStack);
+			player.sendMessage("SoliniaItemID: " + solItem.getId());
+		} catch (CoreStateInitException e)
+		{
+			player.sendMessage("SoliniaItemID: " + "Could not fetch information");
+		} catch (SoliniaItemException e) {
+			// TODO Auto-generated catch block
+			player.sendMessage("SoliniaItemID: " + "Not a solinia item");
+		}
+		return true;
+	}
+}
