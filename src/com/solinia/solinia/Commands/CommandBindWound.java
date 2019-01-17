eb705dd67b10303151a7552a70bee4c7b14cb2d7
Michael Bolland <michaelbollanduk@gmail.com>
2019-01-17 23:00:00 +0000

    check mobs target is not null

--- src/com/solinia/solinia/Commands/CommandBindWound.java
+++ src/com/solinia/solinia/Commands/CommandBindWound.java
@@ -43,6 +43,7 @@ public class CommandBindWound implements CommandExecutor {
 			
 			if (targetmob instanceof Creature)
 			{
+				if (((Creature)targetmob).getTarget() != null)
 				if (((Creature)targetmob).getTarget().getUniqueId().equals(player.getUniqueId()))
 				{
 					player.sendMessage("You cannot bind wound a mob that is currently trying to attack you! Perhaps you meant to target yourself first? [Shift-F]");
