956186e54d0df519143bdb5e1327051a3a5f29e4
Michael Bolland <233287+mixxit@users.noreply.github.com>
2019-08-25 23:59:25 +0100

    send null for group members so smaller messages can be ignored

--- src/com/solinia/solinia/Models/PartyWindow.java
+++ src/com/solinia/solinia/Models/PartyWindow.java
@@ -7,7 +7,7 @@ import com.solinia.solinia.Interfaces.ISoliniaPlayer;
 
 public class PartyWindow {
 	public PartyWindowPlayer Me;
-	public List<PartyWindowPlayer> PartyMembers = new ArrayList<PartyWindowPlayer>();
+	public List<PartyWindowPlayer> PartyMembers;
 	
 	public PartyWindow(PartyWindowPlayer me)
 	{
@@ -20,6 +20,8 @@ public class PartyWindow {
 	
 	public void AddPartyMember(ISoliniaPlayer solPlayer)
 	{
+		if (this.PartyMembers == null)
+			this.PartyMembers = new ArrayList<PartyWindowPlayer>();
 		this.PartyMembers.add(new com.solinia.solinia.Models.PartyWindowPlayer(solPlayer.getFullName(),solPlayer.getSoliniaLivingEntity().getHPRatio()/100D,solPlayer.getSoliniaLivingEntity().getManaRatio()/100D));
 	}
 }
