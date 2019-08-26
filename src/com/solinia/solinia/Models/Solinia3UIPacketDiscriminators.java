6786500f783ab5f53a8ac7fef7689ba940438058
Michael Bolland <233287+mixxit@users.noreply.github.com>
2019-08-26 15:15:40 +0100

    remove generic packet

--- src/com/solinia/solinia/Models/Solinia3UIPacketDiscriminators.java
+++ src/com/solinia/solinia/Models/Solinia3UIPacketDiscriminators.java
@@ -2,7 +2,6 @@ package com.solinia.solinia.Models;
 
 public class Solinia3UIPacketDiscriminators {
 
-	public static final byte GENERIC_MESSAGE = 0;
 	public static final byte VITALS = 1;
 	public static final byte SPELLBOOKPAGE = 2;
 	public static final byte CASTINGPERCENT = 3;
