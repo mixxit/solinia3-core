f52ffff56d8127ce7aaf0249bf95e6a736b3e160
Michael Bolland <233287+mixxit@users.noreply.github.com>
2019-08-29 20:27:19 +0100

    revert

--- src/com/solinia/solinia/Models/Solinia3UIPacketDiscriminators.java
+++ src/com/solinia/solinia/Models/Solinia3UIPacketDiscriminators.java
@@ -2,8 +2,8 @@ package com.solinia.solinia.Models;
 
 public class Solinia3UIPacketDiscriminators {
 
-	public static final byte VITALS = 10;
-	public static final byte SPELLBOOKPAGE = 11;
-	public static final byte CASTINGPERCENT = 12;
-	public static final byte MEMORISEDSPELLS = 13;
+	public static final byte VITALS = 1;
+	public static final byte SPELLBOOKPAGE = 2;
+	public static final byte CASTINGPERCENT = 3;
+	public static final byte MEMORISEDSPELLS = 4;
 }
