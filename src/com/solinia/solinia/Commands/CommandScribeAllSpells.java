58bd6ed50f78c7d2d88ebbc40737b161a746166f
Michael Bolland <233287+mixxit@users.noreply.github.com>
2019-09-20 14:23:11 +0100

    update mod url

---
+++ src/com/solinia/solinia/Commands/CommandScribeAllSpells.java
@@ -0,0 +1,59 @@
+package com.solinia.solinia.Commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import com.solinia.solinia.Adapters.SoliniaPlayerAdapter;
+import com.solinia.solinia.Exceptions.CoreStateInitException;
+import com.solinia.solinia.Exceptions.InvalidNpcSettingException;
+import com.solinia.solinia.Interfaces.ISoliniaNPC;
+import com.solinia.solinia.Interfaces.ISoliniaPlayer;
+import com.solinia.solinia.Managers.StateManager;
+
+public class CommandScribeAllSpells implements CommandExecutor {
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (!(sender instanceof Player) && !(sender instanceof CommandSender))
+			return false;
+		
+		if (!sender.isOp() && !sender.hasPermission("solinia.spawnitem"))
+		{
+			sender.sendMessage("You do not have permission to access this command");
+			return false;
+		}
+		
+		// Args
+		// level
+		
+		if (args.length == 0)
+			return false;
+
+		int level = Integer.parseInt(args[0]);
+		
+		if (args.length == 1)
+		{
+			try
+			{
+				ISoliniaPlayer solPlayer = SoliniaPlayerAdapter.Adapt((Player)sender);
+				if (solPlayer == null || solPlayer.getClassObj() == null)
+					return true;
+				
+				
+				return true;
+			} catch (CoreStateInitException e)
+			{
+				sender.sendMessage(e.getMessage());
+			}
+		}
+		
+		if (args.length < 1)
+		{
+			sender.sendMessage("Insufficient arguments: level");
+			return false;
+		}
+		
+		return true;
+	}
+}
