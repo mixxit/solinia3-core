24e6714dc477af7f1d128757ac8f81cb423ad927
Michael Bolland <233287+mixxit@users.noreply.github.com>
2020-04-30 11:10:54 +0100

    remove player move lock

--- src/com/solinia/solinia/Listeners/PlayerValidatorModListener.java
+++ src/com/solinia/solinia/Listeners/PlayerValidatorModListener.java
@@ -26,73 +26,7 @@ public class PlayerValidatorModListener implements Listener {
 		// TODO Auto-generated constructor stub
 		plugin = solinia3CorePlugin;
 	}
-	
-	@EventHandler
-	public void onPlayerNoMoveUntilModVersionEvent(PlayerMoveEvent event) {
-		if (event.isCancelled())
-			return;
 		
-		if (!event.getFrom().isWorldLoaded())
-			return;
-
-		if (!event.getTo().isWorldLoaded())
-			return;
-
-		if (!event.getFrom().getChunk().isLoaded())
-			return;
-
-		if (!event.getTo().getChunk().isLoaded())
-			return;
-		
-		try
-		{
-			/*if (StateManager.getInstance().getPlayerManager().playerModVersion(event.getPlayer()) == null ||
-					StateManager.getInstance().getPlayerManager().playerModVersion(event.getPlayer()).equals(""))
-			{
-				if (event.getTo().getY() < event.getFrom().getY()) {
-					event.getTo().setX(event.getFrom().getX());
-					event.getTo().setZ(event.getFrom().getZ());
-					event.getTo().setYaw(event.getFrom().getYaw());
-					event.getTo().setPitch(event.getFrom().getPitch());
-
-				} else {
-					event.getTo().setX(event.getFrom().getX());
-					event.getTo().setY(event.getFrom().getY());
-					event.getTo().setZ(event.getFrom().getZ());
-					event.getTo().setYaw(event.getFrom().getYaw());
-					event.getTo().setPitch(event.getFrom().getPitch());
-				}
-				event.getPlayer().spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(ChatColor.GRAY + "* Please wait while the server waits for your client to declare its mod version before moving. For help please ask in /ooc <msg>"));
-				return;
-			}*/
-			
-			ISoliniaPlayer soliniaPlayer = SoliniaPlayerAdapter.Adapt(event.getPlayer());
-			if (soliniaPlayer.getClassObj() == null)
-			{
-				if (event.getTo().getY() < event.getFrom().getY()) {
-					event.getTo().setX(event.getFrom().getX());
-					event.getTo().setZ(event.getFrom().getZ());
-					event.getTo().setYaw(event.getFrom().getYaw());
-					event.getTo().setPitch(event.getFrom().getPitch());
-
-				} else {
-					event.getTo().setX(event.getFrom().getX());
-					event.getTo().setY(event.getFrom().getY());
-					event.getTo().setZ(event.getFrom().getZ());
-					event.getTo().setYaw(event.getFrom().getYaw());
-					event.getTo().setPitch(event.getFrom().getPitch());
-				}
-
-				event.getPlayer().spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(ChatColor.GRAY + "* You must set your race and class before moving. For help please ask in /ooc <msg>"));
-				return;
-			}
-			
-		} catch (CoreStateInitException e)
-		{
-			
-		}
-	}
-	
 	@EventHandler
 	public void onPlayerValidatedModEvent(PlayerValidatedModEvent event) {
 		try {
